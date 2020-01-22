c7c22a93b464015ca50cd0092c0e2f3d8d00cc32
Jon Pither <jon.pither@gmail.com>
2020-01-22 15:03:57 +0000

    Removing unused init from object-store protocol

--- crux-core/src/crux/db.clj
+++ crux-core/src/crux/db.clj
@@ -41,7 +41,6 @@
 ;; hide it.
 ;; tag::ObjectStore[]
 (defprotocol ObjectStore
-  (init [this partial-node options])
   (get-single-object [this snapshot k])
   (get-objects [this snapshot ks])
   (known-keys? [this snapshot ks])
